04e385dcd6a0fa01063d22cca8c1087ada16b48b
tvclena <tvclena@gmail.com>
2025-12-24 23:56:15 -0300

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,10 +1,16 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-    SUPABASE_REST_BASE: process.env.SUPABASE_REST_BASE,
-    SUPABASE_FUNCTIONS_BASE: process.env.SUPABASE_FUNCTIONS_BASE,
-    SUPABASE_REDIRECT_BASE: process.env.SUPABASE_REDIRECT_BASE,
-    APP_NAME: process.env.APP_NAME
-  });
-}
+<script>
+/**
+ * CONFIGURAÇÃO GLOBAL
+ * Esses valores vêm das Environment Variables da Vercel
+ * Elas são injetadas no build automaticamente
+ */
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://fcehmjmtxqmrjkuqlkay.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_iCDWHsq6EguNauQPP49JCg_jUNcz30o",
+
+  API_CREATE_QR: "https://fcehmjmtxqmrjkuqlkay.supabase.co/functions/v1/create",
+  API_REDIRECT_BASE: "https://fcehmjmtxqmrjkuqlkay.supabase.co/functions/v1/r/",
+  REST_QR: "https://fcehmjmtxqmrjkuqlkay.supabase.co/rest/v1/qr_codes",
+  REST_USERS: "https://fcehmjmtxqmrjkuqlkay.supabase.co/rest/v1/usuarios"
+};
+</script>
